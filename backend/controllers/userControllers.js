5e2d97802c495ce9cc7e1b71a5f87ebcb4d13019
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-03-11 12:13:26 +0530

    - added user controllers

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,22 +1,70 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler')
-const User = require('../models/userModel')
+const User = require('../models/userModel');
 
 // @desc    Register User
 // @route   POST /api/users/
 // @access  public
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'User Registered'
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please enter all fields')
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
+
+    if (user) {
+        res.status(201)
+            .json({
+                _id: user.id,
+                name: user.name,
+                email: user.email
+            })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc    Login User
 // @route   POST /api/users/login
 // @access  public
 const loginUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'User Logged in'
-    })
+    const { email, password } = req.body
+
+    // check for user email
+    const user = await User.findOne({ email })
+    const checkPassword = await bcrypt.compare(password, user.password)
+    if (user && checkPassword) {
+        res.status(200)
+            .json({
+                _id: user.id,
+                name: user.name,
+                email: user.email
+            })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @desc    Get User data
